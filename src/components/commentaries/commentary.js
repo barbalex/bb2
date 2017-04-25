e9eb161647f6b12514d84401d6581cb1388ab4c3
Alexander Gabriel <alex@gabriel-software.ch>
2017-04-25 22:22:14 +0200

    mobxify

--- src/components/commentaries/commentary.js
+++ src/components/commentaries/commentary.js
@@ -1,83 +1,71 @@
+// @flow
 import React from 'react'
 import { Button } from 'react-bootstrap'
 import { Base64 } from 'js-base64'
+import { observer, inject } from 'mobx-react'
+import compose from 'recompose/compose'
+import withState from 'recompose/withState'
+import withHandlers from 'recompose/withHandlers'
+
 import Editor from '../editor.js'
 import Meta from '../pages/pageMeta.js'
 
-export default React.createClass({
-  displayName: 'Commentary',
+const metaButtonStyle = {
+  position: 'fixed',
+  bottom: 10,
+  right: 10,
+}
+const enhance = compose(
+  inject(`store`),
+  withState('showMeta', 'changeShowMeta', false),
+  withHandlers({
+    onClickMeta: props => () => props.changeShowMeta(!props.showMeta),
+    onCloseMeta: props => () => props.changeShowMeta(false),
+  }),
+  observer,
+)
 
-  propTypes: {
-    activeCommentary: React.PropTypes.object,
-    editing: React.PropTypes.bool,
-    showMeta: React.PropTypes.bool,
-    onSaveCommentaryArticle: React.PropTypes.func
-  },
+const Commentary = ({
+  activeCommentary,
+  editing,
+  showMeta,
+  onSaveCommentaryArticle,
+  onClickMeta,
+  onCloseMeta,
+}: {
+  activeCommentary: Object,
+  editing: boolean,
+  showMeta: boolean,
+  onSaveCommentaryArticle: () => void,
+  onClickMeta: () => void,
+  onCloseMeta: () => void,
+}) => {
+  const articleEncoded = activeCommentary.article
+  const articleDecoded = Base64.decode(articleEncoded)
 
-  getInitialState() {
-    return {
-      showMeta: false
-    }
-  },
-
-  onClickMeta() {
-    const { showMeta } = this.state
-    this.setState({
-      showMeta: !showMeta
-    })
-  },
-
-  onCloseMeta() {
-    this.setState({
-      showMeta: false
-    })
-  },
-
-  render() {
-    const { activeCommentary, editing, onSaveCommentaryArticle } = this.props
-    const { showMeta } = this.state
-    const articleEncoded = activeCommentary.article
-    const articleDecoded = Base64.decode(articleEncoded)
-    const metaButtonStyle = {
-      position: 'fixed',
-      bottom: 10,
-      right: 10
-    }
-    if (editing) {
-      return (
-        <div
-          className="commentary"
-        >
-          {
-            showMeta &&
-            <Meta
-              doc={activeCommentary}
-              onCloseMeta={this.onCloseMeta}
-            />
-          }
-          <Editor
-            doc={activeCommentary}
-            articleDecoded={articleDecoded}
-            onSaveCommentaryArticle={onSaveCommentaryArticle}
-          />
-          <Button
-            style={metaButtonStyle}
-            onClick={this.onClickMeta}
-          >
-            images
-          </Button>
-        </div>
-      )
-    }
-    const createMarkup = () => ({ __html: articleDecoded })
+  if (editing) {
     return (
-      <div
-        className="commentary"
-      >
-        <div
-          dangerouslySetInnerHTML={createMarkup()}
+      <div className="commentary">
+        {showMeta && <Meta doc={activeCommentary} onCloseMeta={onCloseMeta} />}
+        <Editor
+          doc={activeCommentary}
+          articleDecoded={articleDecoded}
+          onSaveCommentaryArticle={onSaveCommentaryArticle}
         />
+        <Button style={metaButtonStyle} onClick={onClickMeta}>
+          images
+        </Button>
       </div>
     )
   }
-})
+  const createMarkup = () => ({ __html: articleDecoded })
+  return (
+    <div className="commentary">
+      <div dangerouslySetInnerHTML={createMarkup()} />
+    </div>
+  )
+}
+
+Commentary.displayName = 'Commentary'
+
+export default enhance(Commentary)
