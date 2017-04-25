0e86df92d67da4283f0642421bd583be8a62dbd1
Alexander Gabriel <alex@gabriel-software.ch>
2017-04-25 21:34:31 +0200

    mobxify

--- src/components/events/tagsInput.js
+++ src/components/events/tagsInput.js
@@ -1,68 +1,69 @@
-import app from 'ampersand-app'
+// @flow
 import React from 'react'
 import { Glyphicon } from 'react-bootstrap'
+import { observer, inject } from 'mobx-react'
+import compose from 'recompose/compose'
+import withHandlers from 'recompose/withHandlers'
+
 import allTags from './tags.js'
+
+const enhance = compose(
+  inject(`store`),
+  withHandlers({
+    onChangeTag: props => (tag, event, activeEvent) => {
+      const checked = event.target.checked
+      if (checked) {
+        activeEvent.tags.push(tag)
+        props.store.events.saveEvent(activeEvent)
+      } else {
+        activeEvent.tags = activeEvent.tags.filter(_tag => _tag !== tag)
+        props.store.events.saveEvent(activeEvent)
+      }
+    },
+  }),
+  observer,
+)
 
 const tagIcon = option => {
   const top = option.top ? option.top : 0
   const glyphStyle = {
     top,
-    fontSize: '1.5em'
+    fontSize: '1.5em',
   }
   return <Glyphicon glyph={option.iconText} style={glyphStyle} />
 }
-
-const onChangeTag = (tag, event, activeEvent) => {
-  const checked = event.target.checked
-  if (checked) {
-    activeEvent.tags.push(tag)
-    app.Actions.saveEvent(activeEvent)
-  } else {
-    activeEvent.tags = activeEvent.tags.filter(_tag => _tag !== tag)
-    app.Actions.saveEvent(activeEvent)
-  }
-}
-
-const tags = activeEvent => {
-  const options = allTags()
-  return options.map((option, index) => {
-    const checked = activeEvent.tags.includes(option.tag)
-    return (
-      <div key={index} className="form-group event-tag">
-        <label>
-          <input
-            type="checkbox"
-            checked={checked}
-            onChange={event => onChangeTag(option.tag, event, activeEvent)}
-          />
-          {option.iconText && tagIcon(option)}
-          &nbsp;{option.tag}
-        </label>
-      </div>
-    )
-  })
-}
-
 const labelStyle = {
   fontWeight: 'bold',
-  marginBottom: 2
+  marginBottom: 2,
 }
+const options = allTags()
 
-const EventTags = ({ activeEvent }) => (
+const EventTags = ({
+  activeEvent,
+  onChangeTag,
+}: { activeEvent: Object, onChangeTag: () => void }) => (
   <div style={{ marginBottom: 20 }}>
     <div style={labelStyle}>
       Tags
     </div>
     <div className="event-tags">
-      {tags(activeEvent)}
+      {options.map((option, index) => (
+        <div key={index} className="form-group event-tag">
+          <label>
+            <input
+              type="checkbox"
+              checked={activeEvent.tags.includes(option.tag)}
+              onChange={event => onChangeTag(option.tag, event, activeEvent)}
+            />
+            {option.iconText && tagIcon(option)}
+            &nbsp;{option.tag}
+          </label>
+        </div>
+      ))}
     </div>
   </div>
 )
 
 EventTags.displayName = 'EventTags'
 
-EventTags.propTypes = {
-  activeEvent: React.PropTypes.object
-}
-
-export default EventTags
+export default enhance(EventTags)
