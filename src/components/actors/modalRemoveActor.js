6d6b844432e18672d7b90e782282061f121174ef
Alexander Gabriel <alex@gabriel-software.ch>
2017-04-25 22:47:37 +0200

    mobxify

--- src/components/actors/modalRemoveActor.js
+++ src/components/actors/modalRemoveActor.js
@@ -1,7 +1,25 @@
+// @flow
 import React from 'react'
 import { Modal, Button } from 'react-bootstrap'
+import { observer, inject } from 'mobx-react'
+import compose from 'recompose/compose'
+import withHandlers from 'recompose/withHandlers'
 
-const ModalRemoveActor = ({ doc, removeActor }) =>
+const enhance = compose(
+  inject(`store`),
+  withHandlers({
+    remove: props => () => props.store.actors.removeActor(props.doc),
+    abort: props => () => props.store.actors.setShowNewActor(false),
+  }),
+  observer,
+)
+
+const ModalRemoveActor = ({
+  store,
+  doc,
+  remove,
+  abort,
+}: { store: Object, doc: Object, remove: () => void, abort: () => void }) => (
   <div className="static-modal">
     <Modal.Dialog>
       <Modal.Header>
@@ -15,30 +33,17 @@ const ModalRemoveActor = ({ doc, removeActor }) =>
         </p>
       </Modal.Body>
       <Modal.Footer>
-        <Button
-          bsStyle="danger"
-          onClick={() =>
-            removeActor(true)
-          }
-        >
+        <Button bsStyle="danger" onClick={remove}>
           yes, remove!
         </Button>
-        <Button
-          onClick={() =>
-            removeActor(false)
-          }
-        >
+        <Button onClick={abort}>
           no!
         </Button>
       </Modal.Footer>
     </Modal.Dialog>
   </div>
+)
 
 ModalRemoveActor.displayName = 'ModalRemoveActor'
 
-ModalRemoveActor.propTypes = {
-  doc: React.PropTypes.object,
-  removeActor: React.PropTypes.func
-}
-
-export default ModalRemoveActor
+export default enhance(ModalRemoveActor)
