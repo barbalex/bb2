027d1415e3a1f9d71d9b8aac879ec356dd3a4733
Alexander Gabriel <alex@gabriel-software.ch>
2017-04-26 17:51:18 +0200

    flowify

--- src/components/attachedImage.js
+++ src/components/attachedImage.js
@@ -1,3 +1,4 @@
+// @flow
 import app from 'ampersand-app'
 import React from 'react'
 import {
@@ -8,59 +9,45 @@ import {
   InputGroup,
 } from 'react-bootstrap'
 import CopyToClipboard from 'react-copy-to-clipboard'
+
 import getCouchUrl from '../modules/getCouchUrl.js'
+
+const divStyle = {
+  padding: 5,
+}
+const imgStyle = {
+  width: 220,
+}
+const glyphStyle = {
+  position: 'absolute',
+  top: 10,
+  left: 175,
+  fontSize: '2em',
+  color: 'red',
+  cursor: 'pointer',
+}
+const mediaLeftStyle = {
+  position: 'relative',
+}
 
 const AttachedImage = ({
   doc,
   attName,
   urlCopied,
   onCopyUrl,
+}: {
+  doc: Object,
+  attName: string,
+  urlCopied: string,
+  onCopyUrl: () => void,
 }) => {
   const id = doc._id
   const url = `${getCouchUrl()}/${id}/${attName}`
-  const divStyle = {
-    padding: 5
-  }
-  const imgStyle = {
-    width: 220
-  }
-  const glyphStyle = {
-    position: 'absolute',
-    top: 10,
-    left: 175,
-    fontSize: '2em',
-    color: 'red',
-    cursor: 'pointer'
-  }
-  const mediaLeftStyle = {
-    position: 'relative'
-  }
-  const urlCopiedButtonBsStyle = (
-    urlCopied === url ?
-    'success' :
-    'default'
-  )
-  const innerButton = (
-    <CopyToClipboard
-      text={url}
-      onCopy={() => onCopyUrl(url)}
-    >
-      <Button
-        bsStyle={urlCopiedButtonBsStyle}
-      >
-        {urlCopied === url ? 'copied' : 'copy'}
-      </Button>
-    </CopyToClipboard>
-  )
+  const urlCopiedButtonBsStyle = urlCopied === url ? 'success' : 'default'
+
   return (
-    <div
-      key={id}
-      style={divStyle}
-    >
-      <div
-        className="media-left"
-        style={mediaLeftStyle}
-      >
+    <div key={id} style={divStyle}>
+      <div className="media-left" style={mediaLeftStyle}>
         <img
           src={url}
           className="media-object"
@@ -70,23 +57,19 @@ const AttachedImage = ({
         <Glyphicon
           glyph="remove-circle"
           style={glyphStyle}
-          onClick={() =>
-            app.Actions.removePageAttachment(doc, attName)
-          }
+          onClick={() => app.Actions.removePageAttachment(doc, attName)}
         />
       </div>
-      <div
-        className="media-body media-middle"
-      >
+      <div className="media-body media-middle">
         <FormGroup>
           <InputGroup>
-            <FormControl
-              type="text"
-              value={url}
-              disabled
-            />
+            <FormControl type="text" value={url} disabled />
             <InputGroup.Button>
-              {innerButton}
+              <CopyToClipboard text={url} onCopy={() => onCopyUrl(url)}>
+                <Button bsStyle={urlCopiedButtonBsStyle}>
+                  {urlCopied === url ? 'copied' : 'copy'}
+                </Button>
+              </CopyToClipboard>
             </InputGroup.Button>
           </InputGroup>
         </FormGroup>
@@ -96,12 +79,5 @@ const AttachedImage = ({
 }
 
 AttachedImage.displayName = 'AttachedImage'
-
-AttachedImage.propTypes = {
-  doc: React.PropTypes.object,
-  attName: React.PropTypes.string,
-  urlCopied: React.PropTypes.string,
-  onCopyUrl: React.PropTypes.func
-}
 
 export default AttachedImage
