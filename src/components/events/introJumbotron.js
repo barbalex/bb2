ff294eec702bb9cf87bfd5c2a2dda292137c3bdc
Alexander Gabriel <alex@gabriel-software.ch>
2017-04-26 10:29:52 +0200

    eliminate createClass

--- src/components/events/introJumbotron.js
+++ src/components/events/introJumbotron.js
@@ -2,28 +2,29 @@
  * can't convert to es6 function because this is referenced
  */
 
-import React from 'react'
+import React, { Component } from 'react'
 import { Jumbotron } from 'react-bootstrap'
 
-export default React.createClass({
-  displayName: 'IntroJumbotron',
+const jumbotronStyle = {
+  hyphens: 'manual',
+}
+const pStyle = { marginBottom: 0 }
+
+class IntroJumbotron extends Component {
+  displayName: 'IntroJumbotron'
 
   render() {
     return (
-      <Jumbotron
-        className="eventsIntro"
-        style={{
-          hyphens: 'manual'
-        }}
-      >
-        <p style={{ marginBottom: 0 }}>
-          Most mi&shy;grants and re&shy;fu&shy;gees ar&shy;ri&shy;ving in Eu&shy;ro&shy;pe
-          cross the blue bor&shy;­ders of the Eas&shy;­tern and Cen&shy;­tral Me­diterrane­an.
-          The flow is massive and high&shy;ly com&shy;plex. This web&shy;­si&shy;te provides a
-          rough over&shy;­view by co&shy;ve&shy;­ring chro&shy;­no&shy;­lo&shy;­gi&shy;­cal&shy;­ly
-          both ma­ri&shy;­ti­me and political events.
+      <Jumbotron className="eventsIntro" style={jumbotronStyle}>
+        <p style={pStyle}>
+          Most mi­grants and re­fu­gees ar­ri­ving in Eu­ro­pe
+          cross the blue bor­­ders of the Eas­­tern and Cen­­tral Me­diterrane­an.
+          The flow is massive and high­ly com­plex. This web­­si­te provides a
+          rough over­­view by co­ve­­ring chro­­no­­lo­­gi­­cal­­ly
+          both ma­ri­­ti­me and political events.
         </p>
       </Jumbotron>
     )
   }
-})
+}
+export default IntroJumbotron
