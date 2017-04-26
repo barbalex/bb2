984d9778cb8720d9cb55bd6feb98f078d995fd60
Alexander Gabriel <alex@gabriel-software.ch>
2017-04-26 17:45:30 +0200

    mobxify

--- src/components/publications/modalRemovePublication.js
+++ src/components/publications/modalRemovePublication.js
@@ -1,7 +1,25 @@
+// @flow
 import React from 'react'
 import { Modal, Button } from 'react-bootstrap'
+import { observer, inject } from 'mobx-react'
+import compose from 'recompose/compose'
+import withHandlers from 'recompose/withHandlers'
 
-const ModalRemovePublication = ({ doc, removePublication }) =>
+const enhance = compose(
+  inject(`store`),
+  withHandlers({
+    remove: props => (): void => props.removePublication(true),
+    abort: props => (): void => props.removePublication(false),
+  }),
+  observer,
+)
+
+const ModalRemovePublication = ({
+  store,
+  doc,
+  remove,
+  abort,
+}: { store: Object, doc: Object, remove: () => void, abort: () => void }) => (
   <div className="static-modal">
     <Modal.Dialog>
       <Modal.Header>
@@ -15,30 +33,17 @@ const ModalRemovePublication = ({ doc, removePublication }) =>
         </p>
       </Modal.Body>
       <Modal.Footer>
-        <Button
-          bsStyle="danger"
-          onClick={() =>
-            removePublication(true)
-          }
-        >
+        <Button bsStyle="danger" onClick={remove}>
           yes, remove!
         </Button>
-        <Button
-          onClick={() =>
-            removePublication(false)
-          }
-        >
+        <Button onClick={abort}>
           no!
         </Button>
       </Modal.Footer>
     </Modal.Dialog>
   </div>
+)
 
 ModalRemovePublication.displayName = 'ModalRemovePublication'
 
-ModalRemovePublication.propTypes = {
-  doc: React.PropTypes.object,
-  removePublication: React.PropTypes.func
-}
-
-export default ModalRemovePublication
+export default enhance(ModalRemovePublication)
