9d1994894381ed3d073be28164e5a37901d3750d
Alexander Gabriel <alex@gabriel-software.ch>
2017-04-26 12:10:36 +0200

    mobxify

--- src/components/events/eventLink.js
+++ src/components/events/eventLink.js
@@ -1,6 +1,5 @@
-import app from 'ampersand-app'
+// @flow
 import React from 'react'
-import { ListenerMixin } from 'reflux'
 import {
   Row,
   Col,
@@ -8,86 +7,103 @@ import {
   OverlayTrigger,
   Glyphicon,
   FormGroup,
-  FormControl
+  FormControl,
 } from 'react-bootstrap'
+import { observer, inject } from 'mobx-react'
+import compose from 'recompose/compose'
+import withHandlers from 'recompose/withHandlers'
 
-export default React.createClass({
-  displayName: 'EventLink',
+const glyphStyle = {
+  fontSize: '1.5em',
+  color: 'red',
+  cursor: 'pointer',
+}
 
-  propTypes: {
-    activeEvent: React.PropTypes.object,
-    link: React.PropTypes.object,
-    focus: React.PropTypes.bool,
-    index: React.PropTypes.number
-  },
+const enhance = compose(
+  inject(`store`),
+  withHandlers({
+    onChangeUrl: props => (e: Object): void => {
+      // not using action because don't know
+      // how to find this link in activeEvent.links...
+      props.link.url = e.target.value
+    },
+    onBlurUrl: props => (): void => {
+      const { activeEvent, link: oldLink, link: newLink, store } = props
+      const index = activeEvent.links.findIndex(
+        link => link.label === oldLink.label && link.url === oldLink.url,
+      )
+      activeEvent.links[index] = newLink
+      store.events.saveEvent(activeEvent)
+    },
+    onChangeLabel: props => (e: Object): void => {
+      props.link.label = e.target.value
+    },
+    onBlurLabel: props => (): void => {
+      const { activeEvent, link: oldLink, link: newLink, store } = props
+      const index = activeEvent.links.findIndex(
+        link => link.url === oldLink.url && link.label === oldLink.label,
+      )
+      activeEvent.links[index] = newLink
+      store.events.saveEvent(activeEvent)
+    },
+    onRemoveLink: props => (): void => {
+      const { activeEvent, link: linkToRemove, store } = props
+      activeEvent.links = activeEvent.links.filter(
+        link =>
+          link.label !== linkToRemove.label && link.url !== linkToRemove.url,
+      )
+      store.events.saveEvent(activeEvent)
+    },
+  }),
+  observer,
+)
 
-  mixins: [ListenerMixin],
-
-  getInitialState() {
-    return {
-      link: this.props.link
-    }
-  },
-
-  componentDidMount() {
-    this.listenTo(app.eventsStore, this.onEventsStoreChange)
-  },
-
-  onEventsStoreChange() {
-    // this is a bad hack
-    // without it state is not changed when activeEvent changes
-    // > if a link is deleted, the wrong one keeps being shown
-    this.setState({ link: this.props.link })
-  },
-
-  onChangeUrl(e) {
-    const { link } = this.state
-    link.url = e.target.value
-    this.setState({ link })
-  },
-
-  onBlurUrl() {
-    const { activeEvent, link: oldLink } = this.props
-    const { link: newLink } = this.state
-    const index = activeEvent.links.findIndex(
-      link => link.label === oldLink.label && link.url === oldLink.url
-    )
-    activeEvent.links[index] = newLink
-    app.Actions.saveEvent(activeEvent)
-  },
-
-  onChangeLabel(e) {
-    const { link } = this.state
-    link.label = e.target.value
-    this.setState({ link })
-  },
-
-  onBlurLabel() {
-    const { activeEvent, link: oldLink } = this.props
-    const { link: newLink } = this.state
-    const index = activeEvent.links.findIndex(
-      link => link.url === oldLink.url && link.label === oldLink.label
-    )
-    activeEvent.links[index] = newLink
-    app.Actions.saveEvent(activeEvent)
-  },
-
-  onRemoveLink() {
-    const { activeEvent } = this.props
-    const { link: linkToRemove } = this.props
-    activeEvent.links = activeEvent.links.filter(
-      link => link.label !== linkToRemove.label && link.url !== linkToRemove.url
-    )
-    app.Actions.saveEvent(activeEvent)
-  },
-
-  removeLinkGlyph() {
-    const glyphStyle = {
-      fontSize: '1.5em',
-      color: 'red',
-      cursor: 'pointer'
-    }
-    return (
+const EventLink = ({
+  activeEvent,
+  link,
+  focus,
+  index,
+  onChangeUrl,
+  onBlurUrl,
+  onChangeLabel,
+  onBlurLabel,
+  onRemoveLink,
+}: {
+  activeEvent: Object,
+  link: Object,
+  focus: boolean,
+  index: number,
+  onChangeUrl: () => void,
+  onBlurUrl: () => void,
+  onChangeLabel: () => void,
+  onBlurLabel: () => void,
+  onRemoveLink: () => void,
+}) => (
+  <Row key={index}>
+    <Col sm={3} lg={2}>
+      <FormGroup controlId="eventLink">
+        <FormControl
+          type="text"
+          bsSize="small"
+          value={link.label}
+          onChange={onChangeLabel}
+          onBlur={onBlurLabel}
+          autoFocus={focus && !link.label}
+        />
+      </FormGroup>
+    </Col>
+    <Col sm={8} lg={9}>
+      <FormGroup controlId="eventUrl">
+        <FormControl
+          type="url"
+          bsSize="small"
+          value={link.url}
+          onChange={onChangeUrl}
+          onBlur={onBlurUrl}
+        />
+      </FormGroup>
+    </Col>
+    <Col sm={1} lg={1}>
       <OverlayTrigger
         placement="right"
         overlay={
@@ -99,46 +115,13 @@ export default React.createClass({
         <Glyphicon
           glyph="remove-circle"
           style={glyphStyle}
-          onClick={this.onRemoveLink}
+          onClick={onRemoveLink}
         />
       </OverlayTrigger>
-    )
-  },
+    </Col>
+  </Row>
+)
 
-  render() {
-    const { focus, index } = this.props
-    const { link } = this.state
-    const focusLabel = focus && !link.label
+EventLink.displayName = 'EventLink'
 
-    return (
-      <Row key={index}>
-        <Col sm={3} lg={2}>
-          <FormGroup controlId="eventLink">
-            <FormControl
-              type="text"
-              bsSize="small"
-              value={link.label}
-              onChange={event => this.onChangeLabel(event)}
-              onBlur={() => this.onBlurLabel()}
-              autoFocus={focusLabel}
-            />
-          </FormGroup>
-        </Col>
-        <Col sm={8} lg={9}>
-          <FormGroup controlId="eventUrl">
-            <FormControl
-              type="url"
-              bsSize="small"
-              value={link.url}
-              onChange={event => this.onChangeUrl(event)}
-              onBlur={() => this.onBlurUrl()}
-            />
-          </FormGroup>
-        </Col>
-        <Col sm={1} lg={1}>
-          {this.removeLinkGlyph()}
-        </Col>
-      </Row>
-    )
-  }
-})
+export default enhance(EventLink)
