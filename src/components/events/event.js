529dc82cb658bb9060b2a421371bcfa63f8d7fe5
Alexander Gabriel <alex@gabriel-software.ch>
2017-07-04 09:32:11 +0200

    style using styled

--- src/components/events/event.js
+++ src/components/events/event.js
@@ -3,36 +3,36 @@ import React from 'react'
 import { Glyphicon } from 'react-bootstrap'
 import { observer, inject } from 'mobx-react'
 import compose from 'recompose/compose'
+import styled from 'styled-components'
 
 import RemoveEventGlyph from './removeEventGlyph.js'
 import EditEventGlyph from './editEventGlyph.js'
 
-const linkGlyphStyle = {
-  fontSize: `${0.7}em`,
-  paddingRight: 3,
-  verticalAlign: `${10}%`,
-}
-const outerSpanStyle = {
-  paddingLeft: 5,
-}
+const StyledGlyphicon = styled(Glyphicon)`
+  font-size: 0.7em;
+  padding-right: 3px;
+  vertical-align: 10%;
+`
+const OuterSpan = styled.span`padding-left: 5px;`
 
 const enhance = compose(inject(`store`), observer)
 
 const Event = ({ store, event }: { store: Object, event: Object }) => {
   const showEditingGlyphons = !!store.login.email
-  const classNames = event.tags && event.tags.length > 0
-    ? event.tags.map(tag => `event-${tag}`).join(' ')
-    : []
+  const classNames =
+    event.tags && event.tags.length > 0
+      ? event.tags.map(tag => `event-${tag}`).join(' ')
+      : []
 
-  const links = event.links.map((link, key) => (
-    <span key={key} style={outerSpanStyle}>
+  const links = event.links.map((link, key) =>
+    <OuterSpan key={key}>
       {key > 0 && ' '}
       <a href={link.url} target="_blank" rel="noopener noreferrer">
-        <Glyphicon glyph="new-window" style={linkGlyphStyle} />
+        <StyledGlyphicon glyph="new-window" />
         {link.label}
       </a>
-    </span>
-  ))
+    </OuterSpan>
+  )
 
   return (
     <li className={classNames}>
