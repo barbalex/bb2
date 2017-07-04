e97af2b3797d8883594ad1aec34fd53e0009268b
Alexander Gabriel <alex@gabriel-software.ch>
2017-07-04 13:55:29 +0200

    style using styled

--- src/components/events/introJumbotron.js
+++ src/components/events/introJumbotron.js
@@ -7,9 +7,22 @@ import { Jumbotron } from 'react-bootstrap'
 import styled from 'styled-components'
 
 const StyledJumbotron = styled(Jumbotron)`
-  hyphens: manual;
+  hyphens: manual !important;
+  padding-left: 30px !important;
+  padding-right: 30px !important;
+  padding-top: 20px !important;
+  padding-bottom: 15px !important;
+  margin-top: 30px !important;
+  margin-bottom: 20px !important;
+  text-align: center !important;
+  background-color: transparent !important;
+  border: 0 solid #ddd !important;
+  border-radius: 4px !important;
 `
-const P = styled.p`margin-bottom: 0;`
+const P = styled.p`
+  margin-bottom: 0 !important;
+  font-size: 18px !important;
+`
 
 class IntroJumbotron extends Component {
   displayName: 'IntroJumbotron'
--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,56 @@
 // @flow
 import React from 'react'
+import styled from 'styled-components'
+import oceanImage from '../images/ocean.jpg'
 
-const Header = () => (
-  <div className="masthead introHeader">
+const Container = styled.div`
+  margin-top: 42px;
+  height: 150px;
+  padding: 15px;
+  &:before {
+    content: ' ';
+    background-image: url(${oceanImage});
+    display: block;
+    position: absolute;
+    left: 0;
+    top: 51px;
+    width: 100%;
+    height: 150px;
+    z-index: -1;
+    opacity: 1;
+  }
+`
+const IntroTitle = styled.div`
+  margin-top: 20px;
+  font-size: 42px;
+  font-weight: 800;
+  line-height: 46px;
+  text-align: center;
+  color: #fff;
+  text-shadow: 2px 2px 3px black, -2px -2px 3px black, 2px -2px 3px black,
+    -2px 2px 3px black;
+`
+const IntroText = styled.div`
+  text-align: center;
+  font-size: 28px;
+  font-weight: 600;
+  line-height: 30px;
+  color: #fff;
+  text-shadow: 2px 2px 3px black, -2px -2px 3px black, 2px -2px 3px black,
+    -2px 2px 3px black;
+`
+
+const Header = () =>
+  <Container className="masthead introHeader">
     <div className="container">
       <div className="row">
         <div className="col-xs-12 manualHyphens">
-          <div className="introText introTitle">
-            blue borders
-          </div>
-          <div className="introText">
-            central & eastern mediterranean
-          </div>
+          <IntroTitle>blue borders</IntroTitle>
+          <IntroText>central & eastern mediterranean</IntroText>
         </div>
       </div>
     </div>
-  </div>
-)
+  </Container>
 
 Header.displayName = 'Header'
 
