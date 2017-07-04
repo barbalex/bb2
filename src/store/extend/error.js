a809010643a0d6819bcafeeb210a429afd4f9eb4
Alexander Gabriel <alex@gabriel-software.ch>
2017-07-04 11:19:38 +0200

    style using styled

--- src/store/extend/error.js
+++ src/store/extend/error.js
@@ -5,7 +5,8 @@ export default (store: Object): void => {
   extendObservable(store.error, {
     errors: [],
     showError: action('showError', (error: Object): void => {
-      const duration = 10000
+      // const duration = 10000
+      const duration = 100000000
       if (!error) {
         // user wants to remove error messages
         store.error.errors = []
