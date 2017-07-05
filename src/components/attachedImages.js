02039cc28fbaecea23ece7c427301ed1b77f62ab
Alexander Gabriel <alex@gabriel-software.ch>
2017-07-05 10:20:29 +0200

    add observer

--- src/components/attachedImages.js
+++ src/components/attachedImages.js
@@ -1,15 +1,19 @@
 // @flow
 import React from 'react'
+import { observer } from 'mobx-react'
+import compose from 'recompose/compose'
+
 import AttachedImgagesList from './attachedImgagesList'
 import AttachImages from './attachImages'
 
-const AttachedImages = ({ doc }: { doc: Object }) => (
+const enhance = compose(observer)
+
+const AttachedImages = ({ doc }: { doc: Object }) =>
   <div>
     <AttachedImgagesList doc={doc} />
     <AttachImages doc={doc} />
   </div>
-)
 
 AttachedImages.displayName = 'AttachedImages'
 
-export default AttachedImages
+export default enhance(AttachedImages)
