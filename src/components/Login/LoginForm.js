111c3bbb2ad9d08d6e9affb62e279186c764cb55
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2018-12-15 15:05:59 +0100

    use mobx-react-lite

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -10,7 +10,7 @@ import {
   FormControl,
 } from 'react-bootstrap'
 import isObject from 'lodash/isObject'
-import { observer, inject } from 'mobx-react'
+import { observer, inject } from 'mobx-react-lite'
 import compose from 'recompose/compose'
 import withHandlers from 'recompose/withHandlers'
 import withState from 'recompose/withState'
