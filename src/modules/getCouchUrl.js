e5530e33dfb3bc53b41e36cd14ff3d0c0d0f8cf8
Alexander Gabriel <alex.barbalex@gmail.com>
2020-02-04 17:55:19 +0100

    work on docker

--- src/modules/getCouchUrl.js
+++ src/modules/getCouchUrl.js
@@ -1,9 +1,10 @@
-// @flow
 // in development should return local path
 import isDev from 'isdev'
 
-const hostname = isDev
-  ? `${window.location.hostname}:5984/bb`
-  : `${window.location.hostname}/api/bb`
+const url = isDev
+  ? `http://localhost:5984/bb`
+  : `https://api.${window.location.hostname}/bb`
+//: `${window.location.hostname}/api/bb`
 
-export default (): string => `${window.location.protocol}//${hostname}`
+export default () => url
+//export default () => `${window.location.protocol}//${url}`
