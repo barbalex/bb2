876460493cffbbf0df0063986abb46aea199ef18
Alexander Gabriel <alex.barbalex@gmail.com>
2020-02-23 21:24:45 +0100

    revert db url

--- src/modules/getCouchUrl.js
+++ src/modules/getCouchUrl.js
@@ -1,10 +1,15 @@
 // in development should return local path
 import isDev from 'isdev'
 
-const url = isDev
+/*const url = isDev
   ? `http://localhost:5984/bb`
   : //: `https://api.${window.location.hostname}/bb`
     `${window.location.hostname}/api/bb`
 
-export default () => url
-//export default () => `${window.location.protocol}//${url}`
+export default () => url*/
+
+const hostname = isDev
+  ? `${window.location.hostname}:5984/bb`
+  : `${window.location.hostname}/api/bb`
+
+export default (): string => `${window.location.protocol}//${hostname}`
