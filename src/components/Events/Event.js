22d581c437ab2c226f2e3927bcc77b61b9efdc83
Alexander Gabriel <alex.barbalex@gmail.com>
2020-02-29 15:05:29 +0100

    functionalize

--- src/components/Events/Event.js
+++ src/components/Events/Event.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { useContext, useMemo } from 'react'
 import { Glyphicon } from 'react-bootstrap'
-import { observer, inject } from 'mobx-react'
-import compose from 'recompose/compose'
+import { observer } from 'mobx-react-lite'
 import styled from 'styled-components'
 
 import RemoveEventGlyph from './RemoveEventGlyph'
 import EditEventGlyph from './EditEventGlyph'
+import storeContext from '../../storeContext'
 
 const StyledGlyphicon = styled(Glyphicon)`
   font-size: 0.7em;
@@ -16,24 +16,27 @@ const OuterSpan = styled.span`
   padding-left: 5px;
 `
 
-const enhance = compose(inject('store'), observer)
-
-const Event = ({ store, event }) => {
+const Event = ({ event }) => {
+  const store = useContext(storeContext)
   const showEditingGlyphons = !!store.login.email
   const classNames =
     event.tags && event.tags.length > 0
       ? event.tags.map(tag => `event-${tag}`).join(' ')
       : []
 
-  const links = event.links.map((link, key) => (
-    <OuterSpan key={key}>
-      {key > 0 && ' '}
-      <a href={link.url} target="_blank" rel="noopener noreferrer">
-        <StyledGlyphicon glyph="new-window" />
-        {link.label}
-      </a>
-    </OuterSpan>
-  ))
+  const links = useMemo(
+    () =>
+      event.links.map((link, key) => (
+        <OuterSpan key={key}>
+          {key > 0 && ' '}
+          <a href={link.url} target="_blank" rel="noopener noreferrer">
+            <StyledGlyphicon glyph="new-window" />
+            {link.label}
+          </a>
+        </OuterSpan>
+      )),
+    [event.links],
+  )
 
   return (
     <li className={classNames}>
@@ -46,4 +49,4 @@ const Event = ({ store, event }) => {
   )
 }
 
-export default enhance(Event)
+export default observer(Event)
