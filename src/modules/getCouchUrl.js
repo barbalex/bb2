d5557d5737f13dcfa3c2ec2a4ce12b8003480098
Alexander Gabriel <alex.barbalex@gmail.com>
2020-03-03 22:21:48 +0100

    work on backend

--- src/modules/getCouchUrl.js
+++ src/modules/getCouchUrl.js
@@ -1,19 +1,11 @@
 // in development should return local path
 import isDev from 'isdev'
 
-/*const url = isDev
-  ? `http://localhost:5984/bb`
-  : //: `https://api.${window.location.hostname}/bb`
-    `${window.location.hostname}/api/bb`
-
-export default () => url*/
-
 export default () => {
   if (typeof window === `undefined`) return 'localhost:5984/bb'
   const hostname = isDev
     ? `${window.location.hostname}:5984/bb`
     : `api.${window.location.hostname}/bb`
-  //: `${window.location.hostname}/api/bb`
 
   return `${window.location.protocol}//${hostname}`
 }
