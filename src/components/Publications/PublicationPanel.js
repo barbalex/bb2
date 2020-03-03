465e9b020ea8696470712d0457dd80dc46099d8d
Alexander Gabriel <alex.barbalex@gmail.com>
2020-03-03 11:31:04 +0100

    use functions

---
+++ src/components/Publications/PublicationPanel.js
@@ -0,0 +1,179 @@
+import React, {
+  useContext,
+  useRef,
+  useCallback,
+  useEffect,
+  useState,
+} from 'react'
+import { observer } from 'mobx-react-lite'
+import styled from 'styled-components'
+import { Glyphicon, OverlayTrigger, Tooltip } from 'react-bootstrap'
+
+import storeContext from '../../storeContext'
+import Publication from './Publication'
+import ModalRemovePublication from './ModalRemovePublication'
+
+const PanelHeading = styled.div`
+  position: relative;
+`
+const PanelBody = styled.div`
+  max-height: ${typeof window !== `undefined` ? window.innerHeight - 127 : 1}px;
+  overflow-y: auto;
+`
+const ToggleDraftGlyphicon = styled(Glyphicon)`
+  position: absolute !important;
+  right: 40px !important;
+  top: 6px !important;
+  font-size: 1.5em;
+  color: ${props => props['data-color']};
+`
+const RemoveGlyphicon = styled(Glyphicon)`
+  position: absolute !important;
+  right: 8px !important;
+  top: 6px !important;
+  font-size: 1.5em;
+`
+
+const PublicationPanel = ({ category, doc, dIndex }) => {
+  const store = useContext(storeContext)
+  const {
+    activePublication,
+    getPublication,
+    toggleDraftOfPublication,
+  } = store.publications
+
+  const isActivePublication = activePublication
+    ? doc._id === activePublication._id
+    : false
+  const showEditingGlyphons = !!store.login.email
+
+  const [docToRemove, changeDocToRemove] = useState(null)
+
+  const ref = useRef(null)
+  const scrollToActivePanel = useCallback(() => {
+    if (typeof window !== `undefined`) {
+      window.scrollTo({
+        left: 0,
+        top: ref.current ? ref.current.offsetTop - 55 : 55,
+        behavior: 'smooth',
+      })
+    }
+  }, [])
+  useEffect(() => {
+    if (isActivePublication) {
+      window.setTimeout(() => {
+        scrollToActivePanel()
+      }, 50)
+    }
+  }, [isActivePublication, scrollToActivePanel])
+
+  const onClickPublication = useCallback(
+    e => {
+      // prevent higher level panels from reacting
+      e.stopPropagation()
+      const idToGet =
+        !activePublication || activePublication._id !== doc._id ? doc._id : null
+      getPublication(idToGet)
+    },
+    [activePublication, doc._id, getPublication],
+  )
+  const onClickEventCollapse = useCallback(event => {
+    // prevent higher level panels from reacting
+    event.stopPropagation()
+  }, [])
+  const onToggleDraft = useCallback(
+    (doc, event) => {
+      event.preventDefault()
+      event.stopPropagation()
+      toggleDraftOfPublication(doc)
+    },
+    [toggleDraftOfPublication],
+  )
+  const removePublication = useCallback(
+    remove => {
+      if (remove) store.publications.removePublication(docToRemove)
+      changeDocToRemove(null)
+    },
+    [docToRemove, store.publications],
+  )
+  const onRemovePublication = useCallback((docToRemove, event) => {
+    event.preventDefault()
+    event.stopPropagation()
+    changeDocToRemove(docToRemove)
+  }, [])
+
+  // use pure bootstrap.
+  // advantage: can add edit icon to panel-heading
+  return (
+    <div key={dIndex} className="panel panel-default month">
+      <PanelHeading
+        className="panel-heading"
+        role="tab"
+        id={`heading${dIndex}`}
+        onClick={onClickPublication}
+        ref={ref}
+      >
+        <h4 className="panel-title">
+          <a
+            role="button"
+            data-toggle="collapse"
+            data-parent={`#${category}`}
+            href={`#collapse${dIndex}`}
+            aria-expanded="false"
+            aria-controls={`#collapse${dIndex}`}
+          >
+            {doc.title}
+          </a>
+        </h4>
+        {showEditingGlyphons && (
+          <OverlayTrigger
+            placement="top"
+            overlay={
+              <Tooltip id="toggleDraft">
+                {doc.draft ? 'publish' : 'unpublish'}
+              </Tooltip>
+            }
+          >
+            <ToggleDraftGlyphicon
+              glyph={doc.draft ? 'ban-circle' : 'ok-circle'}
+              data-color={doc.draft ? 'red' : 'green'}
+              onClick={onToggleDraft.bind(this, doc)}
+            />
+          </OverlayTrigger>
+        )}
+        {showEditingGlyphons && (
+          <OverlayTrigger
+            placement="top"
+            overlay={<Tooltip id="removeThisPublication">remove</Tooltip>}
+          >
+            <RemoveGlyphicon
+              glyph="remove-circle"
+              onClick={onRemovePublication.bind(this, doc)}
+            />
+          </OverlayTrigger>
+        )}
+      </PanelHeading>
+      {isActivePublication && (
+        <div
+          id={`#collapse${dIndex}`}
+          className="panel-collapse collapse in"
+          role="tabpanel"
+          aria-labelledby={`heading${dIndex}`}
+          onClick={onClickEventCollapse}
+        >
+          <PanelBody className="panel-body">
+            <Publication />
+          </PanelBody>
+        </div>
+      )}
+      {docToRemove && (
+        <ModalRemovePublication
+          doc={docToRemove}
+          removePublication={removePublication}
+        />
+      )}
+    </div>
+  )
+}
+
+export default observer(PublicationPanel)
--- src/components/Publications/PublicationsOfCategory.js
+++ src/components/Publications/PublicationsOfCategory.js
@@ -1,239 +1,42 @@
 //
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
-import { Glyphicon, OverlayTrigger, Tooltip } from 'react-bootstrap'
-import { observer, inject } from 'mobx-react'
-import compose from 'recompose/compose'
-import withState from 'recompose/withState'
-import withHandlers from 'recompose/withHandlers'
+import React, { useContext } from 'react'
+import { observer } from 'mobx-react-lite'
 import styled from 'styled-components'
 
-import Publication from './Publication'
-import ModalRemovePublication from './ModalRemovePublication'
+import PublicationPanel from './PublicationPanel'
+import storeContext from '../../storeContext'
 
-const ToggleDraftGlyphicon = styled(Glyphicon)`
-  position: absolute !important;
-  right: 40px !important;
-  top: 6px !important;
-  font-size: 1.5em;
-  color: ${props => props['data-color']};
-`
-const RemoveGlyphicon = styled(Glyphicon)`
-  position: absolute !important;
-  right: 8px !important;
-  top: 6px !important;
-  font-size: 1.5em;
-`
 const PanelGroup = styled.div`
   margin-bottom: 0 !important;
 `
-const PanelHeading = styled.div`
-  position: relative;
-`
-const PanelBody = styled.div`
-  max-height: ${typeof window !== `undefined` ? window.innerHeight - 127 : 1}px;
-  overflow-y: auto;
-`
 
-const enhance = compose(
-  inject('store'),
-  withState('docToRemove', 'changeDocToRemove', null),
-  withHandlers({
-    onClickPublication: props => (id, e) => {
-      // prevent higher level panels from reacting
-      e.stopPropagation()
-      const { activePublication, getPublication } = props.store.publications
-      const idToGet =
-        !activePublication || activePublication._id !== id ? id : null
-      getPublication(idToGet)
-    },
-    onClickEventCollapse: props => event => {
-      // prevent higher level panels from reacting
-      event.stopPropagation()
-    },
-    onRemovePublication: props => (docToRemove, event) => {
-      event.preventDefault()
-      event.stopPropagation()
-      props.changeDocToRemove(docToRemove)
-    },
-    onToggleDraft: props => (doc, event) => {
-      event.preventDefault()
-      event.stopPropagation()
-      props.store.publications.toggleDraftOfPublication(doc)
-    },
-    removePublication: props => remove => {
-      const { docToRemove, changeDocToRemove, store } = props
-      if (remove) store.publications.removePublication(docToRemove)
-      changeDocToRemove(null)
-    },
-  }),
-  observer,
-)
-
-class PublicationsOfCategory extends Component {
-  componentDidMount() {
-    // somehow on first load the panel does not scroll up far enough
-    // call for more
-    this.scrollToActivePanel('more')
-  }
-
-  componentDidUpdate(prevProps) {
-    if (
-      this.props.store.publications.activePublication &&
-      typeof window !== `undefined`
-    ) {
-      window.setTimeout(() => {
-        this.scrollToActivePanel()
-      }, 200)
-    }
-  }
-
-  scrollToActivePanel = more => {
-    const node = ReactDOM.findDOMNode(this._activePublicationPanel)
-    if (node) {
-      const navWrapperOffsetTop = document.getElementById('nav-wrapper')
-        .offsetTop
-      let reduce = navWrapperOffsetTop > 0 ? navWrapperOffsetTop - 30 : 52
-      // somehow on first load the panel does not scroll up far enough
-      if (more) reduce -= 5
-      if (node.offsetTop && typeof window !== `undefined`) {
-        window.scroll({ top: node.offsetTop - reduce, behavior: 'smooth' })
-      }
-    }
-  }
-
-  removePublicationGlyph = doc => {
-    const { onRemovePublication } = this.props
-
-    return (
-      <OverlayTrigger
-        placement="top"
-        overlay={<Tooltip id="removeThisPublication">remove</Tooltip>}
-      >
-        <RemoveGlyphicon
-          glyph="remove-circle"
-          onClick={onRemovePublication.bind(this, doc)}
-        />
-      </OverlayTrigger>
-    )
-  }
-
-  toggleDraftGlyph = doc => {
-    const { onToggleDraft } = this.props
-    const glyph = doc.draft ? 'ban-circle' : 'ok-circle'
-    const color = doc.draft ? 'red' : 'green'
-
-    return (
-      <OverlayTrigger
-        placement="top"
-        overlay={
-          <Tooltip id="toggleDraft">
-            {doc.draft ? 'publish' : 'unpublish'}
-          </Tooltip>
-        }
-      >
-        <ToggleDraftGlyphicon
-          glyph={glyph}
-          data-color={color}
-          onClick={onToggleDraft.bind(this, doc)}
-        />
-      </OverlayTrigger>
-    )
-  }
-
-  publicationsComponent = category => {
-    const { store, onClickPublication, onClickEventCollapse } = this.props
-    let { publications, activePublication } = store.publications
-    // filter only publication of current category
-    publications = publications.filter(
-      publication => publication.category === category,
-    )
-    publications = publications.sort((a, b) => {
-      if (a.order && b.order) {
-        if (a.order < b.order) return -1
-        return 1
-      }
-      if (a.title < b.title) return -1
+const PublicationsOfCategory = ({ category }) => {
+  const store = useContext(storeContext)
+  let { publications } = store.publications
+  // filter only publication of current category
+  publications = publications.filter(
+    publication => publication.category === category,
+  )
+  publications = publications.sort((a, b) => {
+    if (a.order && b.order) {
+      if (a.order < b.order) return -1
       return 1
-    })
-    return publications.map((doc, dIndex) => {
-      const isActivePublication = activePublication
-        ? doc._id === activePublication._id
-        : false
-      const showEditingGlyphons = !!store.login.email
-      const ref = isActivePublication
-        ? '_activePublicationPanel'
-        : `_publicationPanel${doc._id}`
+    }
+    if (a.title < b.title) return -1
+    return 1
+  })
 
-      // use pure bootstrap.
-      // advantage: can add edit icon to panel-heading
-      return (
-        <div
-          key={dIndex}
-          ref={c => {
-            this[ref] = c
-          }}
-          className="panel panel-default month"
-        >
-          <PanelHeading
-            className="panel-heading"
-            role="tab"
-            id={`heading${dIndex}`}
-            onClick={onClickPublication.bind(this, doc._id)}
-          >
-            <h4 className="panel-title">
-              <a
-                role="button"
-                data-toggle="collapse"
-                data-parent={`#${category}`}
-                href={`#collapse${dIndex}`}
-                aria-expanded="false"
-                aria-controls={`#collapse${dIndex}`}
-              >
-                {doc.title}
-              </a>
-            </h4>
-            {showEditingGlyphons && this.toggleDraftGlyph(doc)}
-            {showEditingGlyphons && this.removePublicationGlyph(doc)}
-          </PanelHeading>
-          {isActivePublication && (
-            <div
-              id={`#collapse${dIndex}`}
-              className="panel-collapse collapse in"
-              role="tabpanel"
-              aria-labelledby={`heading${dIndex}`}
-              onClick={onClickEventCollapse}
-            >
-              <PanelBody className="panel-body">
-                <Publication />
-              </PanelBody>
-            </div>
-          )}
-        </div>
-      )
-    })
-  }
-
-  render() {
-    const { category, docToRemove, removePublication } = this.props
-
-    return (
-      <PanelGroup
-        className="panel-group"
-        id={category}
-        ref={c => {
-          this[category] = c
-        }}
-      >
-        {this.publicationsComponent(category)}
-        {docToRemove && (
-          <ModalRemovePublication
-            doc={docToRemove}
-            removePublication={removePublication}
-          />
-        )}
-      </PanelGroup>
-    )
-  }
+  return (
+    <PanelGroup className="panel-group" id={category}>
+      {publications.map((doc, dIndex) => (
+        <PublicationPanel
+          key={doc._id}
+          category={category}
+          doc={doc}
+          dIndex={dIndex}
+        />
+      ))}
+    </PanelGroup>
+  )
 }
-export default enhance(PublicationsOfCategory)
+export default observer(PublicationsOfCategory)
