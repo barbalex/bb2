de64da2e347f3237f8199a47971a80d305238264
Alexander Gabriel <alex.barbalex@gmail.com>
2020-03-04 10:54:06 +0100

    correct error

--- src/modules/getPathFromDocId.js
+++ src/modules/getPathFromDocId.js
@@ -2,6 +2,9 @@
 export default id => {
   let path = id
   if (path.startsWith('pages_')) path = path.slice(6)
-  path = path.replace(/_/g, '/').replace('monthlyEvents', 'monthly-events')
+  path = path
+    .replace(/_/g, '/')
+    .replace('monthlyEvents', 'monthly-events')
+    .replace('commentaries', 'articles')
   return path
 }
