7ea45c6276936b91f5035968d80ff451e76d0a07
Alexander Gabriel <alex.barbalex@gmail.com>
2021-07-16 13:42:32 +0200

    add CaseSensitivePathsPlugin

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,5 @@
+var CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin')
+
 exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
   if (stage === 'build-html') {
     actions.setWebpackConfig({
@@ -9,6 +11,12 @@ exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
           },
         ],
       },
+    })
+  }
+  // see: https://github.com/gatsbyjs/gatsby/issues/26563#issuecomment-879585547
+  if (stage === 'develop' || stage === 'build-javascript') {
+    actions.setWebpackConfig({
+      plugins: [new CaseSensitivePathsPlugin()],
     })
   }
 }
