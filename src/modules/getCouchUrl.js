5ad1fd7e9baed40882e7e5901384a1a577a80da9
Alexander Gabriel <alex.barbalex@gmail.com>
2022-01-24 00:39:55 +0100

    revert changes to docker settings

--- src/modules/getCouchUrl.js
+++ src/modules/getCouchUrl.js
@@ -1,13 +1,12 @@
 // in development should return local path
-//import isDev from 'isdev'
+import isDev from 'isdev'
 
 export default () => {
   if (typeof window === `undefined`) return 'localhost:5984/bb'
-  // const hostname = isDev
-  //   ? `${window.location.hostname}:5984/bb`
-  //   : `${window.location.hostname}/bb`
-  const hostname = `api.${window.location.hostname}`.replaceAll('.www', '')
-  //const hostname = `api.blue-borders.ch`
+  const hostname = isDev
+    ? `${window.location.hostname}:5984/bb`
+    : `${window.location.hostname}/bb`
+  //const hostname = `api.${window.location.hostname}`.replaceAll('.www', '')
 
   return `${window.location.protocol}//${hostname}`
 }
