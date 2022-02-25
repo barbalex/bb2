83256156e3cb1f66903b7ff642774859e93eb0dc
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2022-02-25 17:14:08 +0100

    add ngos

--- src/pages/sar.js
+++ src/pages/sar.js
@@ -13,6 +13,9 @@ const NgoContainer = styled.div`
   &:not(:first-of-type) {
     margin-top: 20px;
   }
+  &:last-of-type {
+    margin-bottom: 20px;
+  }
 `
 const Title = styled.h3`
   margin: 0;
@@ -108,6 +111,95 @@ const sar = () => (
       <Text>German NGO, 2015 –</Text>
       <Text>{'coop. with Open Arms & Mediterranea (MSH)'}</Text>
       <Text>use of airplanes</Text>
+    </NgoContainer>
+    <NgoContainer>
+      <Title>
+        <a
+          href="https://de.wikipedia.org/wiki/Sea-Eye_4"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Sea Eye 4
+        </a>
+      </Title>
+      <Text>German flag, 2021 –</Text>
+      <Text>3 SAR operations in 2021</Text>
+      <Title>
+        <a
+          href="https://de.wikipedia.org/wiki/Sea-Eye"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Sea Eye
+        </a>
+      </Title>
+      <Text>German NGO, 2015 –</Text>
+    </NgoContainer>
+    <NgoContainer>
+      <Title>
+        <a
+          href="https://de.wikipedia.org/wiki/Open_Arms"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open Arms
+        </a>
+      </Title>
+      <Text>Spanish flag, 2017 –</Text>
+      <Text>3 SAR operations in 2021</Text>
+      <Title>
+        <a
+          href="https://en.wikipedia.org/wiki/Proactiva_Open_Arms"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Proactiva Open Arms
+        </a>
+      </Title>
+      <Text>Spanish NGO, 2015 –</Text>
+      <Text>coop. with Sea Watch and Mediterranea (MSH)</Text>
+    </NgoContainer>
+    <NgoContainer>
+      <Title>
+        <a href="https://resq.it/progetto" target="_blank" rel="noreferrer">
+          ResQ People
+        </a>
+      </Title>
+      <Text>Italian flag, 2020 –</Text>
+      <Text>1 SAR operation in 2021</Text>
+      <Title>
+        <a
+          href="https://www.infomigrants.net/en/post/26362/italian-migrant-rescue-boat-resqpeople-project-launched"
+          target="_blank"
+          rel="noreferrer"
+        >
+          ResQ – People Saving People
+        </a>
+      </Title>
+      <Text>Italian NGO, 2020 –</Text>
+    </NgoContainer>
+    <NgoContainer>
+      <Title>
+        <a
+          href="https://en.wikipedia.org/wiki/Aita_Mari"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Aita Mari
+        </a>
+      </Title>
+      <Text>Spanish flag, 2018 –</Text>
+      <Text>1 SAR operation in 2021</Text>
+      <Title>
+        <a
+          href="https://en.wikipedia.org/wiki/Salvamento_Mar%C3%ADtimo_Humanitario"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Salvamento Maritimo Humanitario - SMH
+        </a>
+      </Title>
+      <Text>Spanish NGO, 2015 –</Text>
     </NgoContainer>
   </Container>
 )
