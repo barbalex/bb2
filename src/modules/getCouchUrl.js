00aef5ad0d2657f7d707fc8b936bf563eec7becc
Alexander Gabriel <alex.barbalex@gmail.com>
2022-02-28 18:44:41 +0100

    optimize

--- src/modules/getCouchUrl.js
+++ src/modules/getCouchUrl.js
@@ -1,12 +1,13 @@
 // in development should return local path
 import isDev from 'isdev'
 
-export default () => {
+const getCouchUrl = () => {
+  // return 'https://blue-borders.ch/bb'
   if (typeof window === `undefined`) return 'localhost:5984/bb'
   const hostname = isDev
     ? `${window.location.hostname}:5984/bb`
     : `${window.location.hostname}/bb`
-  //const hostname = `api.${window.location.hostname}`.replaceAll('.www', '')
-
   return `${window.location.protocol}//${hostname}`
 }
+
+export default getCouchUrl
