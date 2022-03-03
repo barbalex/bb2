ab07e7212d08ce3c76133c986225ef937a907d5b
Alexander Gabriel <alex.barbalex@gmail.com>
2022-03-03 17:16:30 +0100

    access local db in dev

--- src/modules/graphQlUri.js
+++ src/modules/graphQlUri.js
@@ -1,3 +1,13 @@
-const graphQlUri = () => 'https://api.artenliste.ch/v1/graphql'
+const graphQlUri = () => {
+  if (typeof window === 'undefined') {
+    return 'https://api.artenliste.ch/v1/graphql'
+  }
+  const hostnameWithoutWww = window.location.hostname.replace('www.', '')
+  const isLocalhost = hostnameWithoutWww === 'localhost'
+
+  return isLocalhost
+    ? 'http://localhost:8080/v1/graphql'
+    : 'https://api.artenliste.ch/v1/graphql'
+}
 
 export default graphQlUri
