77effede9c01bca1e2a78cee58b1ba0742457fbc
Alexander Gabriel <alex.barbalex@gmail.com>
2022-03-22 14:10:22 +0100

    clean

--- src/modules/existsPermissionError.js
+++ src/modules/existsPermissionError.js
@@ -3,22 +3,17 @@
  * returns true if any of them returns permission denied
  */
 const existsPermissionError = (errors) => {
-  //console.log('existsPermissionError, errors:', errors)
   if (!errors) {
-    //console.log('existsPermissionError, !errors')
     return false
   }
   if (!errors.some) {
-    //console.log('existsPermissionError, !errors.some')
     return false
   }
-  //console.log('existsPermissionError, errors.some')
   const exists = errors.some(
     (error) =>
       error.message.includes('permission denied') ||
       error.message.includes('keine Berechtigung'),
   )
-  //console.log('existsPermissionError, exists:', exists)
   return exists
 }
 
